ab25aa1d9dd24ccfd664e494fbd7cb44c3171c53
+ <+>
2024-05-21 14:02:47 +0300

    cope

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    pass  
+    print(f'greet_programmer')
 
 def greet(name):
     pass
+    print(f'greet , {name}')
 
 def greet_with_default(name="programmer"):
     pass
-
+    print(f'greet_with_default , {name}')
 def add(num1, num2):
     pass
-
+    return num1  +  num2
 def halve(number):
     pass
+    return number /2
